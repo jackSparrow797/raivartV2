a9271e5087bb4486feafb611897fad398c581e5d
Медведев Евгений <medvedev@adv.ru>
2020-01-14 10:51:17 +0400

    add design

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+import 'slick-carousel/slick/slick.scss'
 import './scss/app.scss';
-import './scss/main.scss';
 
+import 'jquery/dist/jquery.min'
+import 'slick-carousel/slick/slick.min'
 import 'bootstrap/js/dist/util';
